5d28e24604df0cfbb07d94729b1e76500294cafc
Chaugummi <felix.mayer@students.fhnw.ch>
2021-06-24 01:21:29 +0200

    400 - user profile picture, username

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:geiger_edu/widgets/NavigationContainer.dart';
+
+import 'package:flutter/foundation.dart';
 
 import 'home_screen.dart';
 
@@ -8,6 +12,7 @@ class ProfileScreen extends StatelessWidget {
   static const routeName = '/profilescreen';
 
   static const bckColor = const Color(0xFF5dbcd2); //0xFFedb879
+  static const borderColor = const Color(0xff0085ff);
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +27,58 @@ class ProfileScreen extends StatelessWidget {
         backgroundColor: bckColor,
       ),
       body: Container(
-        margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30),
+        margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            //simple navigation
-            Container(
-                //TODO ADD PROFILE
-            )
 
+        //** USER IMAGE **
+            Column( children: <Widget>[
+              Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: borderColor,
+                        width: 3,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0)
+                  ),
+                  child: ClipRect(
+                      child:Image.asset("assets/img/profile/default.png", fit: BoxFit.fitHeight))
+              ),
+              Text("Change Avatar",
+                  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20, color: borderColor))
+            ]),
+            SizedBox(height: 40),
+        //** USER NAME INPUT **
+            Column(  children: <Widget>[
+                  Row(//crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                    Image.asset("assets/img/profile/user_icon.png", height: 30 ),
+                    SizedBox(width: 10),
+                    Text("Username", style: TextStyle(fontSize: 20),),
+                    SizedBox(width: 60),
+                    new Flexible(child: SizedBox( width: 200, child: TextField(
+                      controller: new TextEditingController(text: "Clara Kavali"),
+                      decoration: InputDecoration(
+                        //hintText: 'username',
+                        //labelText: 'username'
+                      ),
+                      maxLines: 1,
+                      //maxLength: 14,
+                      onEditingComplete:() { debugPrint('username:'); },)),
+                    )
+            ]),]),
+
+            Container(
+              //TODO: LEARN SCORE
+            ),
+            Container(
+              //TODO: LEARN PROGRESS - API export
+            )
           ],
         ),
 
