82c2b9bd670b28786d2a0c630b4179a30e2fe5fe
Chaugummi <felix.mayer@students.fhnw.ch>
2021-07-19 19:38:44 +0200

    restructured the db

--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,13 +1,71 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:geiger_edu/model/settingObj.dart';
 import 'package:geiger_edu/model/userObj.dart';
+import 'package:geiger_edu/providers/boxes.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-abstract class DB {
-  void init() {
-      //TODO export the hive db
+ Box<User> userBox = Boxes.getUsers();
+ Box<Setting> settingBox = Boxes.getSettings();
+
+class DB {
+  //generate boxes
+
+
+  static void init() async {
+    //** Hive DB Setup **
+    WidgetsFlutterBinding.ensureInitialized();
+    //Initializes Hive with a valid directory in the app files
+    await Hive.initFlutter();
+
+    //Register adapters
+    Hive.registerAdapter(UserAdapter());
+
+    await Hive.deleteBoxFromDisk('users'); //TODO REMOVE IN PRODUCTION ENV
+    await Hive.deleteBoxFromDisk('settings'); //TODO REMOVE IN PRODUCTION ENV
+
+    bool exists = await Hive.boxExists('users');
+    if(!exists){//if it doesnt exist
+      await Hive.openBox<User>('users'); //user table
+      createDefaultUser();
+    }
   }
 
+  static void createDefaultUser(){
+    //add default user to box
+    User defaultUser = new User(userName: 'Turan', userImagePath: 'assets/img/profile/user_icon.png', userScore: 20);
+    userBox.put("default", defaultUser);
+  }
+
+  static User? getDefaultUser(){
+    return userBox.get("default");
+  }
+
+  static void editDefaultUser(String? userName, String? userImagePath, int? userScore){
+    User? tempUser = getDefaultUser();
+
+    if(tempUser!.userName != userName && userName != null)
+    tempUser.userName = userName;
+
+    if(tempUser.userImagePath != userImagePath && userImagePath != null)
+    tempUser.userScore = userScore!;
+
+    if(tempUser.userScore != userScore && userScore != null)
+    tempUser.userImagePath = userImagePath!;
+
+    //persist modified User object to database
+    userBox.put("default", tempUser);
+  }
+
+  static void modifyUserScore(int score){
+    User? tempUser = getDefaultUser();
+    tempUser!.userScore += score;
+    userBox.put("default", tempUser);
+  }
+
+  static void createDefaultSettings(){
+    //add default settings to box
+    Setting defaultSetting = new Setting(darkmode: false, showAlias: true, showScore: false);
+    settingBox.put("settings", defaultSetting);
+  }
 }
