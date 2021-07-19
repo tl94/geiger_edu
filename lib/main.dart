97ddb042e2294017c2a29d936f7fc8af1c9d7c35
Chaugummi <felix.mayer@students.fhnw.ch>
2021-07-19 19:40:11 +0200

    moved the db logic outside the main-file

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:geiger_edu/model/userObj.dart';
-import 'package:geiger_edu/providers/boxes.dart';
 import 'package:geiger_edu/route_generator.dart';
 import 'package:geiger_edu/screens/home_screen.dart';
 import 'package:geiger_edu/services/db.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:geiger_edu/model/userObj.dart';
+
 InAppLocalhostServer localhostServer = new InAppLocalhostServer();
 
 void main() async {
-  //** Hive DB Setup **
-  WidgetsFlutterBinding.ensureInitialized();
-  //Initializes Hive with a valid directory in the app files
-  await Hive.initFlutter();
-
-  //Register adapters
-  Hive.registerAdapter(UserAdapter());
-
-  await Hive.deleteBoxFromDisk('users'); //TODO REMOVE IN PRODUCTION ENV
-
-  bool exists = await Hive.boxExists('users');
-  if(!exists){//if it doesnt exist
-    await Hive.openBox<User>('users'); //user table
-
-    //generate first box
-    final box = Boxes.getUsers();
-    //add default user to box
-    User defaultUser = new User(userName: 'Turan', userImagePath: 'default', userScore: 20);
-    //box.add(defaultUser);
-    box.put("default", defaultUser);
-
-    //printTest
-    print(box.getAt(0)!.userName); //get value at index
-    print(box.get("default")!.userScore);
-
-    defaultUser.userName = 'Felix';
-    defaultUser.save();
-
-    print(box.getAt(0)!.userName);
-  }
+  //** INITIALISE DATABASE **
+  DB.init();
 
   //** LESSON-SERVER **
   localhostServer.start();
