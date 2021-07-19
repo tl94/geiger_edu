32f277114ac86d28c3c7db448387e4266578d678
Chaugummi <felix.mayer@students.fhnw.ch>
2021-07-19 19:39:40 +0200

    removed the variables that are now gotten through the db

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,6 +2,12 @@
 library geiger_edu.globals;
 
 import 'package:flutter/cupertino.dart';
+import 'package:geiger_edu/model/userObj.dart';
+import 'package:geiger_edu/providers/boxes.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+
 final txtColor = const Color(0xff2f4858);
 
 bool appRunning = false;
@@ -18,7 +24,6 @@ String apiKey = '6b4c7cac60be10eddc8890c63817e773';
 //** USER DATA **
 //TODO: Put it inside the local DB
 //TODO: Make a USER Class in model
-String userName = "Clara Kavali";
 String userImg = "assets/img/profile/user_icon.png";
 //User score
 int lessonScore = 225;
